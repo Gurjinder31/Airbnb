d0d196ef47152477141bb6e0fdf1015b1bae3d8f
Gurjinder Singh <31705399+Gurjinder31@users.noreply.github.com>
2021-02-04 00:10:21 -0400

    Update Footer.js

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -5,7 +5,7 @@ function Footer() {
   return (
     <div className="footer">
       <p>© 2020 Airbnb clone! No rights reserved - this is a demo!</p>
-      <p>Privacy · Terms · Sitemap · Company Details</p>
+      <p>Privacy Terms Sitemap · Company Details</p>
     </div>
   );
 }
